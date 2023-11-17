81fe1cbef68d0c24c7483843bcce73fae6aee424
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2023-11-17 19:17:12 +0100

    Add class to scrape bikepacking.com

--- endurance/utils.py
+++ endurance/utils.py
@@ -82,3 +82,143 @@ class RaceLinkScraperDotwatcher:
         self.scrape_dotwatcher_links(base_url, num_pages)
         flat_list = [item for sublist in self.links_races for item in sublist]
         return flat_list
+
+
+
+class BikePackingScraper:
+    def __init__(self, flat_list):
+        self.flat_list = flat_list
+        self.data_frames_list = []
+
+    async def main(self, url):
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto(url, {'timeout': 60000})
+
+        # Get html
+        html = await page.content()
+        await browser.close()
+        return html
+
+    def run_scraper(self):
+        for i in range(len(self.flat_list)):
+            print(f'Getting info from:', self.flat_list[i])
+
+            url = self.flat_list[i]
+            html_response = asyncio.get_event_loop().run_until_complete(self.main(url))
+            soup = BeautifulSoup(html_response, 'html.parser')
+
+            # Names riders ---------------------
+            h2_elements = soup.find_all('h2')
+
+            if url == 'https://bikepacking.com/bikes/2023-ascend-armenia-rigs/':
+                h3_elements = soup.find_all('h3')
+                names_riders = [h3.text for h3 in h3_elements]
+                no_names = ['Inspiration', 'Gear/Reviews', 'The Gear Index', 'Bikepacking Bikes', 'Plan Your Trip', 'Essential Reading', 'Where to Begin', 'By Location', 'By Type', 'By Length (days)']
+                names_riders = [name for name in names_riders if name not in no_names]
+            else:
+                names_riders = [h2.text for h2 in h2_elements]
+
+            dict_names = {'names': names_riders, 'url': url}
+
+            # Demographics ---------------------
+            h4_element = soup.find_all('h4', style=lambda value: value and value.startswith('margin-top:'))
+
+            demographics = []
+            for i in range(len(h4_element)):
+                demographics.append(h4_element[i].text)
+
+            age = []
+            location = []
+
+            for item in demographics:
+                age_match = re.search(r'Age (\d+)', item)
+                if age_match:
+                    age.append(int(age_match.group(1)))
+                else:
+                    age.append('No info')
+
+                location_match = re.search(r'/ (.+)', item)
+                if location_match:
+                    location.append(location_match.group(1))
+                else:
+                    location.append('No info')
+
+            country_list = [re.search(r'\((.*?)\)', item).group(1) if re.search(r'\((.*?)\)', item) else 'No info' for item in location]
+
+            # Get info from kit list ---------------------
+            p_element = soup.find_all('p')
+
+            headers = ['ROUTE:', 'BIKE:', 'BAGS:', 'GEAR HIGHLIGHTS:']
+
+            route_info = []
+            bike_info = []
+            bags_info = []
+            gear_info = []
+
+            kit_list = []
+
+            for i, p_element in enumerate(p_element):
+                paragraph_text = p_element.get_text()
+
+                if any(header in paragraph_text for header in headers):
+                    kit_list.append(paragraph_text)
+
+            info_dict = {header: [] for header in headers}
+
+            for i in range(len(kit_list)):
+                for header in headers:
+                    pattern = re.compile(fr'{re.escape(header)}(.*?)(?=\n|$)', re.DOTALL)
+                    match = pattern.search(kit_list[i])
+
+                    if match:
+                        info = match.group(1).strip()
+                        info_dict[header].append(info)
+                    else:
+                        info_dict[header].append('No info')
+
+            route_info = info_dict['ROUTE:']
+            bike_info = info_dict['BIKE:']
+            bags_info = info_dict['BAGS:']
+            gear_info = info_dict['GEAR HIGHLIGHTS:']
+
+            # Title ---------------------
+            h1_element = soup.find('h1')
+            title = h1_element.text.strip('Rigs of ')
+
+            race_info = {'Name': names_riders,
+                         'Age': age,
+                         'Country': country_list,
+                         'Location': location,
+                         'Bike': bike_info,
+                         'Bags': bags_info,
+                         'Gear': gear_info,
+                         'Route': route_info,
+                         }
+
+            list_lengths = {key: len(value) for key, value in race_info.items()}
+            max_length = max(list_lengths.values())
+
+            for key, length in list_lengths.items():
+                if race_info[key] is not None:
+                    race_info[key] = race_info[key] + ['No info'] * (max_length - length) if length < max_length else race_info[key][:max_length]
+
+            race_info = {
+                'Name': race_info['Name'],
+                'Age': race_info['Age'],
+                'Country': race_info['Country'],
+                'Location': race_info['Location'],
+                'Bike': race_info['Bike'],
+                'Bags': race_info['Bags'],
+                'Gear': race_info['Gear'],
+                'Route': race_info['Route'],
+                'Race': title,
+                'Link': url
+            }
+
+            df_iteration = pd.DataFrame(race_info)
+            self.data_frames_list.append(df_iteration)
+
+        # Concatenate all DataFrames in the list into a single DataFrame
+        df_endurance = pd.concat(self.data_frames_list, ignore_index=True)
+        return df_endurance
